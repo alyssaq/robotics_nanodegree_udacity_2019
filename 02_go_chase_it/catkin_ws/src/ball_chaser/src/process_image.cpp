dbde4d6a6aa3abc53797be1947d4dd8ce5d18ea5
alyssaq <alyssaquek@gmail.com>
2019-02-17 23:42:34 +0800

    process image by subscribing to camera image and requesting client service command_robot

--- 02_go_chase_it/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ 02_go_chase_it/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -8,23 +8,50 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    // Request a service and pass the velocities to it to drive the robot
+    ROS_INFO_STREAM("Driving robot");
+    ROS_INFO("Driving robot with velocity linear:%1.2f, angular:%1.2f", lin_x, ang_z);
+
+    // Request velocity
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
+    if (!client.call(srv))
+        ROS_ERROR("Failed to call service command_robot");
 }
 
 // This callback function continuously executes and reads the image data
-void process_image_callback(const sensor_msgs::Image img)
-{
-
+void process_image_callback(const sensor_msgs::Image img) {
     int white_pixel = 255;
+    int white_ball_pos = -1;
+    int left = static_cast<int>(img.width / 3);
+    int right = left * 2;
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
+    // Loop through each pixel in the image and check if there's a white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
+
+    for (int i = 0; i < img.height * img.step; i++) {
+        if (img.data[i] == white_pixel) {
+            white_ball_pos = i % img.width;
+            break;
+        }
+    }
+
+    // Depending on the white ball position, call the drive_robot function and pass velocities
     // Request a stop when there's no white ball seen by the camera
+    if (white_ball_pos < 0) { // stop
+      drive_robot(0.0, 0.0)
+    } else if (white_ball_pos < left) {
+      drive_robot(0.0, 0.5)
+    } else if (white_ball_pos > right) {
+      drive_robot(0.0, -0.5)
+    } else { // forward
+      drive_robot(0.5, 0.0)
+    }
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     // Initialize the process_image node and create a handle to it
     ros::init(argc, argv, "process_image");
     ros::NodeHandle n;
