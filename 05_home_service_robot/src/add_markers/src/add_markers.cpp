a068a74b8eab31bae6267c63e84ed626c06b8f09
alyssaq <alyssaquek@gmail.com>
2019-04-28 04:39:52 +0000

    Add and delete marker

--- 05_home_service_robot/src/add_markers/src/add_markers.cpp
+++ 05_home_service_robot/src/add_markers/src/add_markers.cpp
@@ -8,6 +8,7 @@ int main( int argc, char** argv )
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 
+  uint32_t state = 0;
   while (ros::ok())
   {
     visualization_msgs::Marker marker;
@@ -20,8 +21,9 @@ int main( int argc, char** argv )
     marker.ns = "basic_shapes";
     marker.id = 0;
 
-    marker.type = visualization_msgs::Marker::CYLINDER;
+    marker.type = visualization_msgs::Marker::SPHERE;//CYLINDER;
 
+    if (state == 0) {
     // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
     marker.action = visualization_msgs::Marker::ADD;
 
@@ -35,23 +37,32 @@ int main( int argc, char** argv )
     marker.pose.orientation.w = 1.0;
 
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 0.5;
-    marker.scale.y = 0.5;
-    marker.scale.z = 0.5;
+    marker.scale.x = 0.25;
+    marker.scale.y = 0.25;
+    marker.scale.z = 0.25;
 
     // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
+    marker.color.g = 0.0f;
+    marker.color.b = 0.3f;
     marker.color.a = 1.0;
 
     marker.lifetime = ros::Duration();
-
+    ROS_INFO("publishing");
     if (!ros::ok()) {
       return 0;
     }
-    marker_pub.publish(marker);
+    
+    state = 1;
+    } else {
+    ROS_INFO("deleting");
+    marker.action = visualization_msgs::Marker::DELETE;
 
+    state = 0;
+    }
+
+    marker_pub.publish(marker);
+    sleep(5);
     r.sleep();
   }
 }
