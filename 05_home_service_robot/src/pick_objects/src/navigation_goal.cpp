342bd10f482d391279f61701a28dc7d4943c69b0
alyssaq <alyssaquek@gmail.com>
2019-04-28 12:46:30 +0000

    Subscribe to amcl_pose

--- 05_home_service_robot/src/pick_objects/src/navigation_goal.cpp
+++ 05_home_service_robot/src/pick_objects/src/navigation_goal.cpp
@@ -2,6 +2,7 @@
 #include <nav_msgs/Odometry.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 /**
  * Callback function executes when new topic data comes.
@@ -15,6 +16,13 @@ void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
   ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
 }
 
+void poseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg) {
+  ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
+  ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+
+}
+
+
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
@@ -23,7 +31,7 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "pick_objects");
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("odom", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("amcl_pose", 1000, poseCallback);
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -36,11 +44,12 @@ int main(int argc, char** argv){
   move_base_msgs::MoveBaseGoal goal;
 
   // set up the frame parameters
-  goal.target_pose.header.frame_id = "robot_footprint";
+  goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 1.0;
+  goal.target_pose.pose.position.x = 7.0;
+  goal.target_pose.pose.position.y = 3.0;
   goal.target_pose.pose.orientation.w = 1.0;
 
    // Send the goal position and orientation for the robot to reach
@@ -52,10 +61,11 @@ int main(int argc, char** argv){
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
+    ROS_INFO("Hooray, the base moved 1 meter forward %1.2f %1.2f", 
+    (float) goal.target_pose.pose.position.x, (float) goal.target_pose.pose.position.y);
   else
     ROS_INFO("The base failed to move forward 1 meter for some reason");
 
-  ros::spin();
+  ros::spinOnce();
   return 0;
 }
