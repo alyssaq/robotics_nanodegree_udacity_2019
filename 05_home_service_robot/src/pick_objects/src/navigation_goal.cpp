2cbffc5274a4770545664ba86d848c5419984487
alyssaq <alyssaquek@gmail.com>
2019-04-28 12:44:09 +0800

    Subscribe to odom

--- 05_home_service_robot/src/pick_objects/src/navigation_goal.cpp
+++ 05_home_service_robot/src/pick_objects/src/navigation_goal.cpp
@@ -1,6 +1,19 @@
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+
+/**
+ * Callback function executes when new topic data comes.
+ * Task of the callback function is to print data to screen.
+ */
+void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+  ROS_INFO("Seq: [%d]", msg->header.seq);
+  ROS_INFO("Position-> x: [%f], y: [%f], z: [%f]", msg->pose.pose.position.x,msg->pose.pose.position.y, msg->pose.pose.position.z);
+  ROS_INFO("Orientation-> x: [%f], y: [%f], z: [%f], w: [%f]", msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+  ROS_INFO("Vel-> Linear: [%f], Angular: [%f]", msg->twist.twist.linear.x,msg->twist.twist.angular.z);
+}
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -8,6 +21,9 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
   ros::init(argc, argv, "pick_objects");
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("odom", 1000, chatterCallback);
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -40,5 +56,6 @@ int main(int argc, char** argv){
   else
     ROS_INFO("The base failed to move forward 1 meter for some reason");
 
+  ros::spin();
   return 0;
 }
