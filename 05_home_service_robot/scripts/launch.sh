86c5bf61280f810e131a2de6d4d8a532e9c1dbc8
alyssaq <alyssaquek@gmail.com>
2019-05-01 14:17:32 +0800

    launch script

--- 05_home_service_robot/scripts/launch.sh
+++ 05_home_service_robot/scripts/launch.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 xterm  -e  " source devel/setup.bash; roslaunch robo world.launch " &
 sleep 5
+xterm  -e  " source devel/setup.bash; roslaunch add_markers add_markers.launch" &
+sleep 5
 xterm  -e  " source devel/setup.bash; roslaunch pick_objects pick_objects.launch" &
